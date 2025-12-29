d493910978b684d452362567146b2bc547e24bfd
MD Sarfaraz <sarfarazm933@gmail.com>
2025-12-29 23:00:39 +0530

    Configure for Vercel deployment (vercel.json, whitenoise, wsgi Fix)

--- price_forecast/price_forecast/settings.py
+++ price_forecast/price_forecast/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-change-me-for-production'
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', 'now.sh', '127.0.0.1', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -81,4 +82,5 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- price_forecast/price_forecast/wsgi.py
+++ price_forecast/price_forecast/wsgi.py
@@ -2,6 +2,15 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
+import sys
+from pathlib import Path
+
+# Add the project directory to sys.path
+path = Path(__file__).resolve().parent.parent
+if str(path) not in sys.path:
+    sys.path.append(str(path))
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'price_forecast.settings')
 
 application = get_wsgi_application()
+app = application
