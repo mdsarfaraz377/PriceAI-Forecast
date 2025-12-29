0a2e508b29a25ac25eacc32735d8063d97777966
MD Sarfaraz <sarfarazm933@gmail.com>
2025-12-29 23:02:34 +0530

    Simplify Vercel entry point and fix path issues

--- vercel_app.py
+++ vercel_app.py
@@ -1,17 +1,4 @@
+from price_forecast.wsgi import app
 
-import os
-import sys
-from pathlib import Path
-
-# Add the project folder to the path
-current_dir = Path(__file__).resolve().parent
-project_dir = current_dir / "price_forecast"
-if str(project_dir) not in sys.path:
-    sys.path.append(str(project_dir))
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'price_forecast.settings')
-
-application = get_wsgi_application()
-app = application
+# Vercel needs the 'app' variable to be exposed
+# This file acts as the entry point
